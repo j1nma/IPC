7a8ffd58ea67bcc10fbb060fd9abcbbb1305ce69
j1nma <j1nma@users.noreply.github.com>
2017-08-25 19:50:58 -0300

    more warming up.

--- getFiles.c
+++ getFiles.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <openssl/md5.h>
 
+// en una mac: gcc getFiles.c -o crsce -I /usr/local/opt/openssl/include -L /usr/local/opt/openssl/lib
+// (tener instalado openssl)
 
 int main(int argc, char **argv) {
 
